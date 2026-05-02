e09d38b74b86e1afdbc395f27d08269673b191d3
DarkenedFractals <darkenedfractals@icloud.com>
2026-05-01 19:16:28 -0700

    Update dependencies

--- src/main/java/com/mc3699/smparch/client/ClientRenderEvents.java
+++ src/main/java/com/mc3699/smparch/client/ClientRenderEvents.java
@@ -7,7 +7,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = SMPArch.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = SMPArch.MODID, value = Dist.CLIENT)
 public class ClientRenderEvents {
 
     @SubscribeEvent
