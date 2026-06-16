69c20510258757a58b36999c27c4165a132d5a75
ariy <something@noreply>
2026-06-16 00:41:27 -0600

    Add's  -Ariy's Archetype  -Ariy's Home Coming Ability  -Ariy's Water Jump Ability

---
+++ src/main/java/com/mc3699/smparch/archetype/ariytwo6/WaterjumpAbility.java
@@ -0,0 +1,35 @@
+package com.mc3699.smparch.archetype.ariytwo6;
+
+import net.mc3699.provenance.ability.foundation.BaseAbility;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+
+public class WaterjumpAbility extends BaseAbility {
+    @Override
+    public float getUseCost() {
+        return 4;
+    }
+
+    @Override
+    public Component getName() {
+        return Component.literal("Water Jump");
+    }
+
+    @Override
+    public boolean canExecute(ServerPlayer serverPlayer) {
+        return serverPlayer.isInWater() && serverPlayer.isCrouching();
+
+    }
+
+    @Override
+    public void execute(ServerPlayer player) {
+        player.setDeltaMovement(0,10,0);
+        player.hurtMarked=true;
+    }
+
+    @Override
+    public ResourceLocation getIcon() {
+        return ResourceLocation.withDefaultNamespace("textures/item/water_bucket.png");
+    }
+}
